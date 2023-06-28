22f510c15ee04aeab5b7a6562da250347823efc5
NathanJMM <iinathanjma@gmail.com>
2023-06-27 17:09:48 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var getComputerChoice = function (){
+const Array = ['Rock', 'Paper', 'Scissors'];
+let x = Math.floor(Math.random() * Array.length);
+console.log(Array[x]);
+return Array[x];
+}
+getComputerChoice();
