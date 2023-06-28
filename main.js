9765b9aacaf9c67ec4b1750cc379993178fb1b50
NathanJMM <iinathanjma@gmail.com>
2023-06-27 17:10:57 -0700

    Create HTML Boilerplate.

--- main.js
+++
@@ -1,7 +0,0 @@
-var getComputerChoice = function (){
-const Array = ['Rock', 'Paper', 'Scissors'];
-let x = Math.floor(Math.random() * Array.length);
-console.log(Array[x]);
-return Array[x];
-}
-getComputerChoice();
