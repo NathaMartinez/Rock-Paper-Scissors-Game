c6e5d8b7a8fe946ee6eff3b8897300f698550745
NathanJMM <iinathanjma@gmail.com>
2023-06-30 11:43:51 -0700

    Added an incremental point system based on boolean playerWin, computerWin variables
    Added a for loop after the 5th round to compare
    the player and computer points.

--- main.js
+++ main.js
@@ -2,32 +2,32 @@
 function getComputerChoice (){
     const Array = ['Rock', 'Paper', 'Scissors'];
     let x = Math.floor(Math.random() * Array.length);
-    console.log("Computer Picked:", Array[x]);
+    console.log("Computer picked:", Array[x]);
     return Array[x].toString().toLowerCase();
     }
 
-    function getPlayerChoice() {
-       let userInput = prompt("Enter Rock, Paper, or Scissors");
-       if (userInput.toString().toLowerCase() == 'rock') {
+function getPlayerChoice() {
+    let userInput = prompt("Enter Rock, Paper, or Scissors");
+    if (userInput.toString().toLowerCase() == 'rock') {
         console.log("You picked: Rock")
+        }
+        else if (userInput.toString().toLowerCase() == 'paper') {
+            console.log("You picked: Paper")
+        }
+        else if (userInput.toString().toLowerCase() == 'scissors') {
+            console.log("You picked: Scissors")
        }
-       else if (userInput.toString().toLowerCase() == 'paper') {
-        console.log("You picked: Paper")
+        else {
+            console.log("Error")
        }
-       else if (userInput.toString().toLowerCase() == 'scissors') {
-        console.log("You picked: Scissors")
-       }
-       else {
-        console.log("Error")
-       }
-       return userInput.toString().toLowerCase();
+        return userInput.toString().toLowerCase();
     }
+
     let playerWin = false;
     let computerWin = false;
 function playRound() {
     const playerSelection = getPlayerChoice();
     const computerSelection = getComputerChoice();
-    var playerPoint = 0;
     if (playerSelection == computerSelection){
         console.log("Draw");
         playerWin = false;
@@ -40,8 +40,6 @@ function playRound() {
             "beats", playerSelection);
             playerWin = false;
             computerWin = true;
-           
-            console.log("player points:", playerPoint);
         } else if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
                    (playerSelection == 'paper' && computerSelection == 'rock')  ||
                    (playerSelection == 'scissors' && computerSelection == 'paper')){
@@ -49,23 +47,34 @@ function playRound() {
             "beats", computerSelection,);
             computerWin = false;
             playerWin = true;
-            playerPoint += 1;
         } else {
+            playerWin = false;
+            computerWin = false;
             (console.log("invalid Input. Enter Rock, Paper, or Scissors."));
         }
     }
-}
-    
-    console.log(playRound());
-    
+}   
     function game(){
         var playerPoint = 0;
-        for(let i = 0; i< 5; i++) {
+        var computerPoint = 0;
+        for(let i = 1; i < 6; i++) {
         playRound();
         if (playerWin == true){
             playerPoint += 1;
-            console.log("player points:", playerPoint)
-        }  
+        } if (computerWin == true){
+            computerPoint += 1;
+        }
+        console.log("Round", i) 
+        console.log("Player points:", playerPoint, "\nComputer Points:", computerPoint,"\n-------------------------")
+        }
+        for(i = 5; i < 6; i++){
+            if (playerPoint > computerPoint){
+                console.log("Player Wins.");
+            } else if (playerPoint < computerPoint){
+                console.log("Computer Wins.");
+            } else if (playerPoint == computerPoint){
+                console.log("Its a Tie.");
         }
     }
+}
     console.log(game());
