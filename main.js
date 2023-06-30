9cf1c61f695a227198d648406b863060f6efc26b
NathanJMM <iinathanjma@gmail.com>
2023-06-30 11:15:33 -0700

    Add game function to loop through playRound 5 times
    Removed parameters from playRound function. Moved userInput variable
    to playRound function.
    This allows the userInput to be prompted each iteration

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 
-var getComputerChoice = function (){
+function getComputerChoice (){
     const Array = ['Rock', 'Paper', 'Scissors'];
     let x = Math.floor(Math.random() * Array.length);
     console.log("Computer Picked:", Array[x]);
     return Array[x].toString().toLowerCase();
     }
+
+    function getPlayerChoice() {
        let userInput = prompt("Enter Rock, Paper, or Scissors");
        if (userInput.toString().toLowerCase() == 'rock') {
         console.log("You picked: Rock")
@@ -18,34 +20,52 @@ var getComputerChoice = function (){
        else {
         console.log("Error")
        }
-
-function playRound(playerSelection, computerSelection) {
-    let playerPoint = 0;
-    let computerPoint = 0;
+       return userInput.toString().toLowerCase();
+    }
+    let playerWin = false;
+    let computerWin = false;
+function playRound() {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    var playerPoint = 0;
     if (playerSelection == computerSelection){
         console.log("Draw");
+        playerWin = false;
+        computerWin = false;
     } else if (playerSelection != computerSelection){
         if ((playerSelection == 'rock' && computerSelection == 'paper') ||
             (playerSelection == 'paper' && computerSelection == 'scissors') ||
             (playerSelection == 'scissors' && computerSelection == 'rock')){
             console.log("Computer Wins.\n", computerSelection[0].toUpperCase() + computerSelection.substring(1),
             "beats", playerSelection);
-            playerPoint++;
-            console.log(playerPoint)
+            playerWin = false;
+            computerWin = true;
+           
+            console.log("player points:", playerPoint);
         } else if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
                    (playerSelection == 'paper' && computerSelection == 'rock')  ||
                    (playerSelection == 'scissors' && computerSelection == 'paper')){
             console.log("Player Wins.\n", playerSelection[0].toUpperCase() + playerSelection.substring(1), 
             "beats", computerSelection,);
-            computerPoint++;
-            console.log(computerPoint)
-        } else{
+            computerWin = false;
+            playerWin = true;
+            playerPoint += 1;
+        } else {
             (console.log("invalid Input. Enter Rock, Paper, or Scissors."));
         }
     }
 }
-    const playerSelection = userInput.toString().toLowerCase();
-    const computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
-
-   
+    
+    console.log(playRound());
+    
+    function game(){
+        var playerPoint = 0;
+        for(let i = 0; i< 5; i++) {
+        playRound();
+        if (playerWin == true){
+            playerPoint += 1;
+            console.log("player points:", playerPoint)
+        }  
+        }
+    }
+    console.log(game());
